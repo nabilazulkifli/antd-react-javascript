974e2984028dafb37ed16fff13abb88830e8a625
Han Sheng <hanshengliang@outlook.com>
2022-03-15 00:24:26 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  const [theme, setTheme] = useState('PURPLE');
+
+  useEffect(() => {
+    if (theme === "DEFAULT") {
+      require('./theme/default/defaultTheme.less')
+    } else {
+      require('./theme/purple/purpleTheme.less')
+    }
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Header</h1>
+    </>
   );
 }
 
